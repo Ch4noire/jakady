8cea5348025d21aa2eb4f924d933e7a4b35fb80e
Ch4noire <tiphanielallement@gmail.com>
2025-06-23 18:36:14 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 document.getElementById("creer").onclick = () => {
   window.location.href = "creer-fond.html";
 };
-document.getElementById("gerer").onclick = () => alert("Gérer mes fonds - simulation à venir");
+document.getElementById("gerer").onclick = () => {
+  window.location.href = "gerer-fonds.html";
+};
 document.getElementById("infos").onclick = () => alert("Mes informations - simulation à venir");
 document.getElementById("exporter").onclick = () => alert("Exporter un fond - simulation à venir");
