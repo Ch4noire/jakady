d6af0370b4c80bf5c08ca3840f7adbe0acddf88d
Ch4noire <tiphanielallement@gmail.com>
2025-06-23 18:17:57 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-document.getElementById("creer").onclick = () => alert("Créer un fond - simulation à venir");
-document.getElementById("gerer").onclick = () => alert("Gérer mes fonds - simulation à venir");
-document.getElementById("infos").onclick = () => alert("Mes informations - simulation à venir");
-document.getElementById("exporter").onclick = () => alert("Exporter un fond - simulation à venir");
+document.getElementById("creer").onclick = () => {
+  window.location.href = "creer-fond.html";
+};
